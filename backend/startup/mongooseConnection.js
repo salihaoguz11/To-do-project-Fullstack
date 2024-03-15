a109a80cee8d9ed5f342731aaeb9ebfe9adb1022
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-03-15 12:00:15 +0000

    started

---
+++ backend/startup/mongooseConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { CustomError } = require("../errors/customError");
+
+const mongooseConnection = async () => {
+  try {
+    if (!process.env.MONGODB_URI)
+      throw new CustomError("Mongodb url is required");
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("db connected");
+  } catch (error) {
+    console.log("db not connected");
+    throw new CustomError("db connection is failed");
+  }
+};
+
+module.exports = { mongooseConnection };
